d003506ab703984c5c39ac41593637f02e5e3a74
auraecosystem <sandraisom@icloud.com>
2026-05-26 00:24:33 +0100

    Create install.sh

---
+++ Bin/install.sh
@@ -0,0 +1,7 @@
+# Create a new sprite
+$ sprite create my-openclaw-builder
+# Clone the repo
+git clone https://github.com/theoctopusperson/openclaw-sprite-builder.git ~/openclaw-builder
+# Run the script
+$ bash ~/openclaw-builder/setup.sh
+# Make a million OpenClaws
