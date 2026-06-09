d86fa80e6722f5598d2a90975ad334c11ff9b616
auraecosystem <sandraisom@icloud.com>
2026-06-09 21:47:32 +0100

    Create init.swift

---
+++ init.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+/// An extension that facilitates the display of sloths in user interfaces.
+public extension Image {
+    /// Create an image from the given sloth.
+    ///
+    /// Use this initializer to display an image representation of a
+    /// given sloth.
+    ///
+    /// ```swift
+    /// let iceSloth = Sloth(name: "Super Sloth", color: .blue, power: .ice)
+    ///
+    /// var body: some View {
+    ///     Image(iceSloth)
+    ///         .resizable()
+    ///         .aspectRatio(contentMode: .fit)
+    ///     Text(iceSloth.name)
+    /// }
+    /// ```
+    ///
+    /// ![A screenshot of an ice sloth, with the text Super Sloth underneath.](iceSloth)
+    ///
+    /// This initializer is useful for displaying static sloth images.
+    /// To create an interactive view containing a sloth, use ``SlothView``.
+    init(_ sloth: Sloth) {
+        self.init("\(sloth.power)-sloth")
+    }
+}
