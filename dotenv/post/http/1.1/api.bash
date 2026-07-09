93f3b6e3d061bcca92f7d81621c270a1d52a9e98
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-09 04:53:29 +0100

    Update api.bash

--- dotenv/post/http/1.1/api.bash
+++ dotenv/post/http/1.1/api.bash
@@ -1,6 +1,6 @@
 curl "https://generativelanguage.googleapis.com/v1beta/interactions" \
   -H 'Content-Type: application/json' \
-  -H "x-goog-api-key: AIzaSyAvrxOyAVzPVcnzxuD0mjKVDyS2bNWfC10" \
+  -H "x-goog-api-key: "" \
   -X POST \
   -d '{
     "model": "gemini-3.5-flash",
